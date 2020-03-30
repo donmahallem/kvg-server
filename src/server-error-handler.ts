9fa866fd872f1c1785953c1953f18733c552ab05
donmahallem <donmahallem@users.noreply.github.com>
2020-03-30 22:57:18 +0200

    feat(server-error-handler): add error reporting

--- src/server-error-handler.ts
+++ src/server-error-handler.ts
@@ -6,6 +6,15 @@ import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 export const serverErrorHandler: ErrorRequestHandler =
     (err: any, req: Request, res: Response, next: NextFunction): void => {
         // tslint:disable-next-line:no-console
+        if (err.statusCode) {
+            res.status(err.statusCode)
+                .json({
+                    error: true,
+                    message: err.message,
+                    statusCode: err.statusCode,
+                });
+            return;
+        }
         console.error(err);
-        res.status(500).json({ error: true });
+        res.status(500).json({ error: true, statusCode: 500 });
     };
